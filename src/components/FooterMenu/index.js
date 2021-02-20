11c56d8d1393bd4a413852e9512693bf3a4b52cf
besogogoladze <69896613+besogogoladze@users.noreply.github.com>
2021-02-20 10:51:54 +0100

    sectioTwo damatebulia

--- src/components/FooterMenu/index.js
+++ src/components/FooterMenu/index.js
@@ -5,8 +5,8 @@ import './style.css';
 
 
 export default function FooterMenu(props) {
-    return <div className='col-2 text'>
-        <h6>{props.title}</h6>
+    return <div key="props" className='col-2 text'>
+        <h6 key="title">{props.title}</h6>
         {props.items.map(item => {
             return <p>{item}</p>
         })}
--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,10 +1,12 @@
 import SectionOne from './sectionOne/SectionOne'
+import SectionTwo from './sectionTwo/SectionTwo'
 
 
 function Home() {
   return (
       <div>
           <SectionOne/>
+          <SectionTwo/>
       </div>
   );
 }
